d356cdb73ce6016d37b016fb7b811afb9c63cff1
Jamey Kirk <jameykirk@gmail.com>
2021-04-19 02:34:42 +0900

    job now done

--- dark_navbar.js
+++ dark_navbar.js
@@ -1,24 +1,25 @@
 window.onload = function() {stickyNav()};
-  
-  // nav bar
-  var navbar = document.getElementById("navbar");
-  var sticky = navbar.offsetTop;
-  
-  function stickyNav() {
+
+// nav bar
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function stickyNav() {
     if (window.pageYOffset >= sticky) {
-      navbar.classList.add("sticky")
+        navbar.classList.add("sticky")
     } else {
-      navbar.classList.remove("sticky");
+        navbar.classList.remove("sticky");
     }
-  }
+}
 
-  // night mode
-  var onOffBtn = document.querySelector("#onOff");
+// night mode
+var onOffBtn = document.querySelector("#onOff");
 
-  //var viewState = document.querySelector()
-  onOffBtn.addEventListener("click", changeState); 
-  
-  function changeState (e) {
+//var viewState = document.querySelector()
+onOffBtn.addEventListener("click", changeState); 
+
+// on click of the "dark" button the entire page changes from dark to light
+function changeState (e) {
     var navLinks = document.querySelectorAll("#navbar a");
     var githubLogo = document.querySelectorAll(".github");
     if (e.target.value == "light") {
@@ -55,4 +56,4 @@ window.onload = function() {stickyNav()};
         document.querySelector(".button-primary").style = "color: #EEE; background-color: rgba(50, 50, 50, 85%); border: solid rgba(50, 50, 50, 85%) 0.2rem";
         document.querySelector(".ping").style = "filter: invert(0)";
     }
-  }
+}
